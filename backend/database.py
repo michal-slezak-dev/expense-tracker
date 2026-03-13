9740a6515ec0fb880060eb767527a7e83505d773
Michal <slezak.michal04@gmail.com>
2026-03-13 21:31:39 +0100

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,7 @@ data_dir = "data" if os.path.exists("data") else "."
 postgres_file_name = "database.db"
 postgres_url = f"postgresql+psycopg2:///{os.path.join(data_dir, postgres_file_name)}"
 
-connect_args = {"check_same_thread": False} # allows multiuple threads to access the database
+connect_args = {"check_same_thread": False} # allows multiuple threads to access the database!
 engine = create_engine(postgres_url, echo=True, connect_args=connect_args)
 
 def create_db_and_tables():

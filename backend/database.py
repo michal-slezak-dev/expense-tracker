c10e48a3b25e22442b54386dde408db996ae83f7
Michal <slezak.michal04@gmail.com>
2026-03-13 21:21:11 +0100

    Add database conf.

---
+++ backend/database.py
@@ -0,0 +1,17 @@
+from sqlmodel import SQLModel, create_engine, Session
+from sqlalchemy.engine import Engine
+import os
+
+data_dir = "data" if os.path.exists("data") else "."
+postgres_file_name = "database.db"
+postgres_url = f"postgresql+psycopg2:///{os.path.join(data_dir, postgres_file_name)}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(postgres_url, echo=True, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session() -> Session:
+    with Session(engine) as session:
+        yield session

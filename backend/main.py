3251e99bcc31a379d66260a628f6aaa6d2e55a96
Michal <slezak.michal04@gmail.com>
2026-04-04 21:53:34 +0200

    Add base category endpoints - create & read

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from routers import expenses, categories
+from contextlib import asynccontextmanager
+from database import create_db_and_tables
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
--- backend/routers/categories.py
+++ backend/routers/categories.py
@@ -1,3 +1,46 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
+from database import get_Session
+from pydantic import BaseModel
+from typing import List, Optional
+from sqlmodel import Session, select
+from models import Category
 
 router = APIRouter(prefix="/categories", tags=["categories"])
+
+class CreateCategory(BaseModel):
+    name : str
+
+class CategoryResponse(BaseModel):
+    id : int
+    name : str
+
+# add category
+@router.post('/category', response_model=CategoryResponse)
+async def create_category(category_request : CreateCategory, session : Session = Depends(get_Session)):
+    try:
+        category = Category(
+        name=category_request.name,
+        )
+
+        session.add(category)
+        session.commit()
+        session.refresh(category)
+
+        return category
+
+    except Exception as e:
+        session.delete(category)
+        session.commit()
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+# get categories
+@router.get('/category', response_model=List[Category])
+async def get_categories(session : Session = Depends(get_Session)):
+    statement = select(Category)
+    categories = session.exec(statement).all()
+
+    return categories
+
+# update category
+# delete category

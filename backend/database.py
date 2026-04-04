556015aba862c45e9df8ed7269f3c18bba0ec0f6
Michal <slezak.michal04@gmail.com>
2026-04-04 21:52:54 +0200

    Finish models and db connection

--- backend/database.py
+++ backend/database.py
@@ -1,17 +1,13 @@
 from sqlmodel import SQLModel, create_engine, Session
-from sqlalchemy.engine import Engine
 import os
 
-data_dir = "data" if os.path.exists("data") else "."
-postgres_file_name = "database.db"
-postgres_url = f"postgresql+psycopg2:///{os.path.join(data_dir, postgres_file_name)}"
+postgres_url = 'postgresql+psycopg2://postgres:postgres@localhost:5432/expense_tracker'
 
-connect_args = {"check_same_thread": False} # allows multiuple threads to access the database!
-engine = create_engine(postgres_url, echo=True, connect_args=connect_args)
+engine = create_engine(postgres_url, echo=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-def get_session() -> Session:
+def get_Session():
     with Session(engine) as session:
         yield session
--- backend/models.py
+++ backend/models.py
@@ -4,11 +4,11 @@ from sqlmodel import Field, Relationship, SQLModel
 
 # User - id, username, email, password_hash, created_at, updated_at
 # Category - id, name
-# Expense - id, amount, description, date, created_at, updated_at
+# Expense - id, amount, description, expense_date, created_at, updated_at
 
-class User(SQLModel):
+class User(SQLModel, table=True):
     id : Optional[int] = Field(default=None, primary_key=True) # SERIAL by default
-    username : str = Field(unique=True, index=True, nullable=False)
+    username : str = Field(unique=True, nullable=False, index=True)
     email : str = Field(unique=True, nullable=False)
     password_hash : str = Field(nullable=False)
     created_at : datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
@@ -16,22 +16,23 @@ class User(SQLModel):
 
     expenses : List['Expense'] = Relationship(back_populates='user')
 
-class Category(SQLModel):
+class Category(SQLModel, table=True):
     id : Optional[int] = Field(default=None, primary_key=True)
-    name : str = Field(unique=True, index=True, nullable=False)
+    name : str = Field(unique=True, nullable=False, index=True)
 
     expenses : List['Expense'] = Relationship(back_populates='category')
 
-class Expense(SQLModel):
+
+class Expense(SQLModel, table=True):
     id : Optional[int] = Field(default=None, primary_key=True)
     amount : float = Field(default=0, decimal_places=2)
     description : Optional[str] = Field(default=None)
-    date : date = Field(default_factory=date.today())
+    expense_date : date = Field(default_factory=date.today)
     created_at : datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    updated_at : datetime = Field(default=None)
+    updated_at : Optional[datetime] = Field(default=None)
 
     category_id : Optional[int] = Field(default=None, foreign_key='category.id', ondelete='SET NULL')
-    user_id : Optional[int] = Field(default=None, foreign_key='user.id', ondelete='CASCADE')
+    user_id : int = Field(foreign_key='user.id')
 
     category : Optional[Category] = Relationship(back_populates='expenses')
-    user : Optional[User] = Relationship(back_populates='expenses')
+    user : User = Relationship(back_populates='expenses')
